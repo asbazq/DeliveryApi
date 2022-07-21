49e415c7d073c3a06d9cb2e794d40f67f49a1757
asbazq <107836678+asbazq@users.noreply.github.com>
2022-07-21 11:42:38 +0900

    Delete springcore directory

--- springcore/src/main/java/com/sparta/springcore/repository/ProductRepository.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.springcore.repository;
-import com.sparta.springcore.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> { }
